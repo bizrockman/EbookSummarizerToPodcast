0096a5e949dc0c7c818c7bd23b6ce084146f64e1
Danny Gerst <d.gerst@bizrock.de>
2024-06-13 17:53:21 +0200

    Fixed a bug that makes the create button appear to early.

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,7 +180,7 @@ def draw_main_app():
                     st.session_state.selected_chapters = selected_chapters
             st.session_state.chapter_select_clicked = False
 
-    if st.button("Erstellen"):
+    if len(st.session_state.selected_chapters) > 0 and st.button("Erstellen"):
         st.session_state.button_clicked = True
         chapter_selection_container.empty()
         st.rerun()
